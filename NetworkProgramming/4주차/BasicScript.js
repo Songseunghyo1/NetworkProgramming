0e1072741c6d04aae62ab31d46b5e029de654df0
Songseunghyo1 <tmdgy15@gmail.com>
2018-03-29 11:13:33 +0900

    add 3, 4 week's lecture

---
+++ NetworkProgramming/4주차/BasicScript.js
@@ -0,0 +1,10 @@
+var buttonSend = document.getElementById('send-button');
+var buttonStop = document.getElementById('stop-button');
+
+buttonSend.onclick = function clickSend() {
+  console.log('Button Clicked');
+};
+
+buttonStop.onclick = function clickStop() {
+    console.log('Button Clicked')
+};
